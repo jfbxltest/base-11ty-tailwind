a299de9b51e976d031ab7a4e5fac29854b06fcbf
1402jelorand <jeanfebxl@hotmail.fr>
2023-02-05 23:10:49 +0100

    tailwind ok upload fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html, njk}"],
   theme: {
+    fontFamily: {
+      sans:['Open Sans','Helvetica','Arial','Lucida','sans-serif'],
+      jost: ['Jost', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+      josefin: ['"Josefin Sans"', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
